3a58d16961f961d30057b4233f8a08a00fde689f
ethermol <pmostert@live.com>
2017-11-16 18:36:57 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -26,6 +26,6 @@ class minecraft (
   service {'minecraft':
     ensure => running,
     enable => true,
-    require => [Package['Java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraf.service']],
+    require => [Package[Java],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraf.service']],
   }
 }
