242abf6abe6f815cdefe6848ec53b57265dc805e
ethermol <pmostert@live.com>
2017-11-16 15:20:30 +0100

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,11 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.docker.vm':}
   dockeragent::node {'db.docker.vm':}
+  host {'web.docker.vm':
+    ensure => present,
+    ip     => '172.18.0.4',
+  }
+  host {'db.docker.vm':
+    ensure => present,
+    ip     => '172.18.0.3',
 }
