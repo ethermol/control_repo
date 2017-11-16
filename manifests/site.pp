f49315a2aec5fc10fe632965150feba003f92ff2
ethermol <pmostert@live.com>
2017-11-16 14:53:19 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,10 @@ node default {
 }
 node 'ip-172-29-10-232.mirabeau.cloud' {
   include role::master_server
+  file {'/root/README':
+    ensure => file,
+    content => $fqdn,
+  }
 }
 node /^web/ {
   include role::app_server
